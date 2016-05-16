a155750e8d98954cfc0ebdffb706f64c0c418b39
jorgeluis91x <jorge_luis91x@hotmail.com>
2016-05-16 14:53:39 -0500

    token refresh sin bases de datos

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,3 +1,4 @@
+"use strict"
 var promise = require('bluebird');
 var options = {
   // Initialization Options
@@ -8,6 +9,77 @@ var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://localhost:5432/pet';
 var db = pgp(connectionString);
 
+///////////prueba/////////
+
+///////////////
+module.exports = {
+  user: {
+    updateOrCreate: function(user, cb) {
+      // db dummy, we just cb the user
+      cb(null, user);
+    },
+    authenticate: function(username, password, cb) {
+      // database dummy - find user and verify password
+      if (username === 'devils name' && password === '666') {
+        cb(null, {
+          id: 666,
+          firstname: 'devils',
+          lastname: 'name',
+          email: 'devil@he.ll',
+          verified: true
+        });
+      } else {
+        cb(null, false);
+      }
+    }
+  },
+  client: {
+    // db dummy for clients
+    clients: [],
+    clientCount: 0,
+    updateOrCreate: function(data, cb) {
+      let id = this.clientCount++;
+      this.clients[id] = {
+        id: id,
+        userId: data.user.id
+      };
+      cb(null, {
+        id: id
+      });
+    },
+    storeToken: function(data, cb) {
+      this.clients[data.id].refreshToken = data.refreshToken;
+      cb();
+    },
+    findUserOfToken: function(data, cb) {
+      if(!data.refreshToken){
+        return cb(new Error('invalid token'));
+      }
+      for (let i = 0; i < this.clients.length; i++) {
+        if (this.clients[i].refreshToken === data.refreshToken) {
+          return cb(null, {
+            id: this.clients[i].userId,
+            clientId: this.clients[i].id
+          });
+        }
+      }
+      cb(new Error('not found'));
+    },
+    rejectToken: function(data, cb){
+      for (let i = 0; i < this.clients.length; i++) {
+        if (this.clients[i].refreshToken === data.refreshToken) {
+          this.clients[i] = {};
+          return cb();
+        }
+      }
+      cb(new Error('not found'));
+    }
+  }
+};
+
+
+////////////////////////
+/*
 const db1 = {
 	updateOrCreate: function  (user, cb){
 		cb(null,user);
@@ -51,7 +123,7 @@ const db1 = {
 };
 
 
-
+*/
 
 function encriptar(user, pass) {
    var crypto = require('crypto');
@@ -139,9 +211,9 @@ function signin(req, res,next){
 
 
 
-
+/*
 module.exports = {
 	login: login,
 	signin: signin,
 	db1: db1
-}
+}*/
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var users = require('../queries/users');
+//var users = require('../queries/users');
 
+
+var db = require('../queries/users');
 
 var promise = require('bluebird');
+var crypto = require('crypto')
 var options = {
   // Initialization Options
   promiseLib: promise
@@ -20,24 +23,52 @@ const SECRET = 'server secret';
 const passport = require('passport');  
 const Strategy = require('passport-local');
 const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
 const authenticate = expressJwt({
   secret: SECRET
 });
+const TOKENTIME = 120 * 60; // in seconds
 
+
+// server responses //
+//////////////////////
+const respond = {
+  auth: function(req, res) {
+    res.status(200).json({
+      user: req.user,
+      token: req.token
+    });
+  },
+  token: function(req, res) {
+    res.status(201).json({
+      token: req.token
+    });
+  },
+  reject: function(req, res){
+    res.status(204).end();
+  }
+};
 
 //////////////
 // passport //
 //////////////
 
+/*
 passport.use(new Strategy({
    usernameField: 'email',
    passwordField: 'password'
 },
 function(email, password, done) {
-   users.db1.authenticate(email,password,done);
+   db.user.authenticate(email,password,done);
 }));
+*/
 
-
+passport.use(new Strategy(
+  function(username, password, done) {
+    db.user.authenticate(username, password, done);
+  }
+));
+/*
 router.get('/registrar', function(req, res) {
   res.sendfile('./views/login.html');
 });
@@ -50,31 +81,66 @@ router.post('/login1',passport.initialize(), passport.authenticate(
   'local', {
     session: false,
     scope: []
-  }), serialize, generateToken, respond);
+  }), serializeUser, generateAccessToken, respond.auth);
+
+router.get('/me', authenticate, function(req, res) {
+  res.status(200).json(req.user);
+});
+*/
+router.post('/login2', passport.initialize(), passport.authenticate(
+    'local', {
+      session: false,
+      scope: []
+    }), serializeUser, serializeClient, generateAccessToken,
+  generateRefreshToken, respond.auth);
+
+
+
+//////////new///
+////////////
+router.post('/token', validateRefreshToken, generateAccessToken,respond.token);
+router.post('/token/reject', rejectToken, respond.reject);
 
 router.get('/me', authenticate, function(req, res) {
   res.status(200).json(req.user);
 });
 
 
-
 /////////////
 ////helper///
 /////////////
 
-const jwt = require('jsonwebtoken');
 
-function serialize(req, res, next) { 
+
+function serializeUser(req, res, next) { 
 console.log("serialize"); 
-  users.db1.updateOrCreate(req.user, function(err, user){
+  db.user.updateOrCreate(req.user, function(err, user){
     if(err) {return next(err);}
     // we store the updated information in req.user again
     req.user = {
-      id: user.id_user
+      id: user.id
     };
     next();
   });
 }
+
+function serializeClient(req, res, next) {
+  if (req.query.permanent === 'true') {
+    db.client.updateOrCreate({
+      user: req.user
+    }, function(err, client) {
+      if (err) {
+        return next(err);
+      }
+      // we store information needed in token in req.user
+      req.user.clientId = client.id;
+      next();
+    });
+  } else {
+    next();
+  }
+}
+/*
 function generateToken(req, res, next) { 
 console.log("generateToken"); 
   req.token = jwt.sign({
@@ -91,6 +157,59 @@ function respond(req, res) {
     user: req.user,
     token: req.token
   });
+}*/
+
+function validateRefreshToken(req, res, next) {
+  db.client.findUserOfToken(req.body, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    req.user = user;
+    next();
+  });
 }
+
+function rejectToken(req, res, next) {
+  db.client.rejectToken(req.body, next);
+}
+
+
+
+
+
+
+//////////////////////
+// token generation //
+//////////////////////
+function generateAccessToken(req, res, next) {
+  req.token = req.token ||  {};
+  req.token.accessToken = jwt.sign({
+    id: req.user.id,
+    clientId: req.user.clientId
+  }, SECRET, {
+    expiresIn: TOKENTIME
+  });
+  next();
+}
+
+function generateRefreshToken(req, res, next) {
+  if (req.query.permanent === 'true') {
+    req.token.refreshToken = req.user.clientId.toString() + '.' + crypto.randomBytes(
+      40).toString('hex');
+    db.client.storeToken({
+      id: req.user.clientId,
+      refreshToken: req.token.refreshToken
+    }, next);
+  } else {
+    next();
+  }
+}
+
+//////////////////////
+
+
+
+
+
 
 module.exports = router;
